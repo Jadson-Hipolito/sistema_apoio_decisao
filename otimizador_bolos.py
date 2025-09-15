237d212f04452e0e9fbbb80d49a5fda6906ef6b7
Jadson Hipólito de Almeida <142536589+Jadson-Hipolito@users.noreply.github.com>
2025-09-15 09:39:26 -0300

    Update otimizador_bolos.py

--- otimizador_bolos.py
+++ otimizador_bolos.py
@@ -6,14 +6,13 @@ model = LpProblem(name="otimizacao-bolos", sense=LpMaximize)
 # Variáveis de decisão
 x = LpVariable(name="chocolate", lowBound=10, upBound=60, cat='Integer')  # chocolate
 y = LpVariable(name="creme", lowBound=0, upBound=40, cat='Integer')       # creme
-z = LpVariable(name="morango", lowBound=0, upBound=30, cat='Integer')     # morango
 
 # Função objetivo: maximizar o lucro
-model += 3 * x + 1 * y + 2 * z, "Lucro_total"
+model += 3 * x + 1 * y, "Lucro_total"
 
 # Restrições
-model += 2 * x + 3 * y + 2 * z <= 180, "Tempo_maquinario"  # tempo total
-model += x + y + z >= 30, "Producao_minima"               # produção mínima
+model += 2 * x + 3 * y <= 180, "Tempo_maquinario"  # tempo total
+model += x + y >= 20, "Producao_minima"               # produção mínima
 
 # Resolver o problema
 model.solve()
@@ -21,5 +20,4 @@ model.solve()
 # Resultados
 print(f"Quantidade ideal de bolos de chocolate: {x.value()}")
 print(f"Quantidade ideal de bolos de creme: {y.value()}")
-print(f"Quantidade ideal de bolos de morango: {z.value()}")
 print(f"Lucro total: R${value(model.objective)}")
